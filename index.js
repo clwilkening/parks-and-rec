5167f5f8429b8fe832a8928f48dedccc6e8f1687
clwilkening <clw13clw13@gmail.com>
2018-10-04 22:55:47 -0400

    add music functionality, background logic

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ document.addEventListener("DOMContentLoaded", function(event) {
     sound.addEventListener('click', selectSound);
   });
   document.querySelector('.play').addEventListener('click', play);
+  document.querySelector('.pause').addEventListener('click', pause);
+  document.addEventListener('keypress', handleEnter);
 });
 
 const selectSound = function() {
@@ -13,10 +15,42 @@ const selectSound = function() {
   };
 };
 
-const play = function () {
-  console.log('clicked play')
-  const select = document.querySelectorAll('active');
+const play = () => {
+  const select = document.querySelectorAll('.active');
+  if (select.length === 0) alert('pick a sound first!');
   select.forEach(sound => {
-    console.log(sound)
+    const key = sound.getAttribute("data-key");
+    const audio = document.querySelector(`audio[data-key="${key}"]`);
+    audio.currentTime = 0;
+    audio.play();
   });
+};
+
+const pause = () => {
+  document.querySelectorAll('audio').forEach(audio => {
+    audio.pause();
+  });
+};
+
+const handleEnter = function(e) {
+  var key = e.which || e.keyCode;
+  if (key !== 13) return;
+  const input = document.querySelector('.search').value;
+  if (input.length === 0) alert("Hmm. That name isn't famliar. Try another first name.");
+  let theChosenOne = null;
+  characters.forEach(character => {
+    if (!theChosenOne) {
+      if (input.trim().toLowerCase() === character) {
+        theChosenOne = character;
+        console.log(document.querySelector('body'))
+        document.querySelector('body').style.background = `url(media/${character}.png) no-repeat center center fixed`;
+        document.querySelector('.juke-box').style.backgroundColor = `rgba(156, 204, 101,.3)`
+        return;
+      };
+    };
+  });
+  if (!theChosenOne) alert("Hmm. That name isn't famliar. Try another first name.");
 }
+
+const characters = ['ron', 'leslie', 'tom', 'donna', 'ann', 'chris', 'ben', 'andy'];
+
