79d84c391a98b93db4fbcf2ad754694891d2f31a
clwilkening <clw13clw13@gmail.com>
2018-10-04 21:21:48 -0400

    select sounds, some styling

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
 document.addEventListener("DOMContentLoaded", function(event) { 
-  alert('sup')
+  document.querySelectorAll('.sound').forEach(sound => {
+    sound.addEventListener('click', selectSound);
+  });
+  document.querySelector('.play').addEventListener('click', play);
 });
+
+const selectSound = function() {
+  if (this.classList.contains('active')) {
+    this.classList.remove('active');
+  } else {
+    this.classList.add('active') ;
+  };
+};
+
+const play = function () {
+  console.log('clicked play')
+  const select = document.querySelectorAll('active');
+  select.forEach(sound => {
+    console.log(sound)
+  });
+}
